23d5a09fc808e9ee4afdf47fbf8ee1a8e9c10bcd
Miguel Sánchez Blanco <Putodruida@Putodruida-ASUS>
2016-10-19 09:02:16 +0200

    Ejercio 2 Boletin 4

---
+++ Boletin4_2/src/boletin4_2/Boletin4_2.java
@@ -0,0 +1,10 @@
+package boletin4_2;
+
+public class Boletin4_2 {
+    
+    public static void main(String[] args) {
+        Satelite novoSat = new Satelite(98,26,52);
+        novoSat.verPosicion();
+    }
+    
+}
